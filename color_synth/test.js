8ae58ca6985746b8a29d617ac9dedbb6fc408797
notryanb <notryanb@gmail.com>
2015-05-20 12:33:31 -0400

    Add separate oscillator wave selection.

--- color_synth/test.js
+++ color_synth/test.js
@@ -6,48 +6,66 @@ $().ready(function(){
 
 
 // More Experimenting
-$('#sin-btn').click(function(){
-    synth.set({wave:"sin", mul:1});
+$('#sin1-btn').click(function(){
+    osc1Wave = "sin";
   });
 
-$('#pulse-btn').click(function(){
-    // synth.set({wave:"square"});
-    synth.set({wave:"pulse", mul:0.25});
+$('#sin2-btn').click(function(){
+    osc2Wave = "sin";
   });
 
-$('#saw-btn').click(function(){
-    // synth.set({wave:"square"});
-    synth.set({wave:"saw", mul:0.25});
+$('#pulse1-btn').click(function(){
+    osc1Wave = "pulse";
   });
 
-$('#fami-btn').click(function(){
-    // synth.set({wave:"square"});
-    synth.set({wave:"fami", mul:0.50});
+$('#pulse2-btn').click(function(){
+    osc2Wave = "pulse";
   });
 
-$('#other-btn').click(function(){
-    // synth.set({wave:"square"});
-    pulser = "sin";
-    famiwhami = "saw";
+$('#saw1-btn').click(function(){
+    osc1Wave = "saw";
+  });
+
+$('#saw2-btn').click(function(){
+    osc2Wave = "saw";
+  });
+
+$('#fami1-btn').click(function(){
+    osc1Wave = "fami";
+  });
+
+$('#fami2-btn').click(function(){
+    osc2Wave = "fami";
   });
 
 
 });
 
+
+
+
+// SYNTH 2-----------
+var osc1Wave = "pulse";
+var osc2Wave = "fami";
+
+
 var synth2 = T("SynthDef").play();
 
-var pulser = "pulse";
-var famiwhami = "fami";
-
 synth2.def = function(opts) {
-  var osc1, osc2, env;
-  osc1 = T(pulser, {freq:opts.freq         , mul:0.25});
-  osc2 = T(famiwhami, {freq:opts.freq*2, mul:0.20});
-  env  = T("linen", {s:40, r:150, lv:0.5}, osc1, osc2);
-  return env.on("asdr", opts.doneAction).bang();
+  var osc1, osc2, lfo, env;
+
+  lfo = T("sin", {freq: "250ms", mul:3, add:opts.freq}).kr();
+
+  
+  osc1 = T(osc1Wave, {freq:opts.freq       , mul:0.25});
+  osc2 = T(osc2Wave, {freq:opts.freq * 10, mul:0.20});
+  env  = T("linen", {s:40, r:1050, lv:0.5}, osc1, osc2);
+  return env.on("ended", opts.doneAction).bang();
 };
 
-var synth = T("OscGen", {wave:"fami", mul:1}).play();
+// -------------------
+
+
 
 
 var keydict = T("ndict.key");
@@ -56,18 +74,18 @@ T("keyboard").on("keydown", function(e) {
   var midi = keydict.at(e.keyCode);
   if (midi) {
     boxSelect(midi);
-    console.log(midi);
-    var freq = midicps.at(midi);
-    synth.noteOnWithFreq(freq, 50);
-    // synth2.noteOn(midi, 50);
+    // console.log(midi);
+    // var freq = midicps.at(midi);
+
+    synth2.noteOn(midi, 50);
   }
 }).on("keyup", function(e) {
   var midi = keydict.at(e.keyCode);
   if (midi) {
     boxUnselect(midi);
-    synth.noteOff(midi, 100);
   }
 }).start();
+
 
 function boxSelect(midi) {
   $('#_'+midi).css('opacity', '0.5');
