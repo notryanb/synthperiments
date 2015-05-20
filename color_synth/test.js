43adc823576d6e5a885b8472cabf952127c34a86
notryanb <notryanb@gmail.com>
2015-05-20 15:28:04 -0400

    Refactor waveform and LFO jQuery

--- color_synth/test.js
+++ color_synth/test.js
@@ -1,82 +1,57 @@
 "use strict";
 
 $().ready(function(){ 
-// var LFO = T("sin", {freq:"250ms",  mul:5, add:880}).kr();
 
 
 
-// More Experimenting
-$('#sin1-btn').click(function(){
-    osc1Wave = "sin";
-  });
+    $(".wave").click(function(e){
+      var $this = $(this);
+      var newWave = $this.data('wave');
 
-$('#sin2-btn').click(function(){
-    osc2Wave = "sin";
-  });
+      if ($this.hasClass('osc1')) { 
+        waveForm.osc1Wave = newWave; }
+      if ($this.hasClass('osc2')) { 
+        waveForm.osc2Wave = newWave; }
+    });
 
-$('#square1-btn').click(function(){
-    osc1Wave = "square";
-  });
+    $('#lfo1-btn').click(function(){
+        if (!waveForm.lfo1Switch) {
+          waveForm.lfo1Switch = true;
+          this.innerHTML = "LFO On";
+        } else {
+          waveForm.lfo1Switch = false;
+          this.innerHTML = "LFO Off";
+        }
+      });
 
-$('#square2-btn').click(function(){
-    osc2Wave = "square";
-  });
+    $('#lfo2-btn').click(function(){
+        if (!waveForm.lfo2Switch) {
+          waveForm.lfo2Switch = true;
+          this.innerHTML = "LFO On";
+        } else {
+          waveForm.lfo2Switch = false;
+          this.innerHTML = "LFO Off";
+        }
+      });
 
-$('#saw1-btn').click(function(){
-    osc1Wave = "saw";
-  });
-
-$('#saw2-btn').click(function(){
-    osc2Wave = "saw";
-  });
-
-$('#fami1-btn').click(function(){
-    osc1Wave = "fami";
-  });
-
-$('#fami2-btn').click(function(){
-    osc2Wave = "fami";
-  });
-
-$('#off1-btn').click(function(){
-    osc1Wave = "";
-  });
-
-$('#off2-btn').click(function(){
-    osc2Wave = "";
-  });
-
-$('#lfo1on-btn').click(function(){
-    lfo1Switch = true;
-  });
-
-$('#lfo1off-btn').click(function(){
-    lfo1Switch = false;
-  });
-
-$('#lfo2on-btn').click(function(){
-    lfo2Switch = true;
-  });
-
-$('#lfo2off-btn').click(function(){
-    lfo2Switch = false;
-  });
 });
 
 
-
-
 // SYNTH 2-----------
-var osc1Wave = "sin";
-var osc2Wave = "sin";
-var lfo1Switch = false;
-var lfo2Switch = false;
+
+// Defaults values
+var waveForm = {
+  "osc1Wave": "sin",
+  "osc2Wave": "sin",
+  "lfo1Switch": false,
+  "lfo2Switch": false
+}
 
 
 // Returns FX with freq or just freq.
 var fx = function (opts) {
-  if (lfo1Switch) { return lfoModule(opts);  }
-  else if (lfo2Switch) { return lfoModule(opts);  }
+  if (waveForm.lfo1Switch) { return lfoModule(opts);  }
+  else if (waveForm.lfo2Switch) { return lfoModule(opts);  }
   else { return opts.freq; } 
 };
 
@@ -85,13 +60,14 @@ var lfoModule = function(opts) {
     return lfo;
 };
 
+
 var synth2 = T("SynthDef").play();
 
 synth2.def = function(opts) {
   var osc1, osc2, env;
   
-  osc1 = T(osc1Wave, {freq: fx(opts), mul:0.25});
-  osc2 = T(osc2Wave, {freq: fx(opts), mul:0.20});
+  osc1 = T(waveForm.osc1Wave, {freq: fx(opts), mul:0.25});
+  osc2 = T(waveForm.osc2Wave, {freq: fx(opts), mul:0.20});
   env  = T("linen", {s:40, r:1050, lv:0.5}, osc1, osc2);
   return env.on("ended", opts.doneAction).bang();
 };
@@ -100,7 +76,7 @@ synth2.def = function(opts) {
 
 
 
-
+// ----- Key Controller ------------
 var keydict = T("ndict.key");
 var midicps = T("midicps");
 T("keyboard").on("keydown", function(e) {
